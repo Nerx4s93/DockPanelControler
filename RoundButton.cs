8c3659e780fd1b0b2397a31308dd4993593e2a44
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-07-29 06:37:31 +0300

    Кнопка с закруглёнными краями

---
+++ RoundButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DockPanelControler
+{
+    public class RoundButton : Button
+    {
+        public RoundButton()
+        {
+            DoubleBuffered = true;
+        }
+
+        #region Свойства
+
+        public float OutlineWidth { get; set; } = 2f;
+
+        public Color OutlineColor { get; set; } = Color.DarkGray;
+
+        public int TopRadius { get; set; } = 5;
+
+        public int BottomRadius { get; set; } = 5;
+
+        #endregion
+
+        public void DrawPath(Graphics graphics)
+        {
+            var rectangle = PathBuilder.GetRecrangleFFromSize(Size);
+            var graphicsPath = PathBuilder.GetRoundRectanglePath(rectangle, TopRadius, BottomRadius);
+
+            Region = new Region(graphicsPath);
+
+            using (Pen pen = new Pen(OutlineColor, OutlineWidth))
+            {
+                pen.Alignment = PenAlignment.Inset;
+                graphics.DrawPath(pen, graphicsPath);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawPath(e.Graphics);
+        }
+    }
+}
