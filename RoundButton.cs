b4e00834796f463c2065eb7a6809c7630ed7d1e9
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-07-30 08:50:12 +0300

    Кастомизация кнопки

--- RoundButton.cs
+++ RoundButton.cs
@@ -1,11 +1,15 @@
 ﻿using System.Drawing.Drawing2D;
 using System.Drawing;
 using System.Windows.Forms;
+using System;
 
 namespace DockPanelControler
 {
     public class RoundButton : Button
     {
+        private bool _mouseEnter;
+        private bool _mouseDown;
+
         public RoundButton()
         {
             DoubleBuffered = true;
@@ -13,9 +17,17 @@ namespace DockPanelControler
 
         #region Свойства
 
-        public float OutlineWidth { get; set; } = 2f;
+        public float OutlineWidth { get; set; } = 3f;
 
         public Color OutlineColor { get; set; } = Color.DarkGray;
+
+        public Color OutlineColorOnMouseEnter { get; set; } = Color.Gray;
+
+        public Color OutlineColorOnClick { get; set; } = Color.DimGray;
+
+        public Color BackColorOnMouseEnter { get; set; } = Color.FromArgb(224, 224, 224);
+
+        public Color BackColorOnClick { get; set; } = Color.FromArgb(200, 200, 200);
 
         public int TopRadius { get; set; } = 5;
 
@@ -23,8 +35,41 @@ namespace DockPanelControler
 
         #endregion
 
-        public void DrawPath(Graphics graphics)
+        protected override void OnMouseEnter(EventArgs e)
         {
+            _mouseEnter = true;
+            Invalidate();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            _mouseEnter = false;
+            Invalidate();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            _mouseDown = true;
+            Invalidate();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            _mouseDown = false;
+            Invalidate();
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            var outlineColor = _mouseDown ?
+                OutlineColorOnClick :
+                _mouseEnter ? OutlineColorOnMouseEnter : OutlineColor;
+            var backColor = _mouseDown ?
+                BackColorOnClick :
+                _mouseEnter ? BackColorOnMouseEnter : BackColor;
+
+            graphics.Clear(backColor);
+
             var rectangle = PathBuilder.GetRecrangleFFromSize(Size);
             var graphicsPath = PathBuilder.GetRoundRectanglePath(rectangle, TopRadius, BottomRadius);
 
@@ -32,7 +77,7 @@ namespace DockPanelControler
 
             if (OutlineWidth != 0)
             {
-                using (Pen pen = new Pen(OutlineColor, OutlineWidth))
+                using (Pen pen = new Pen(outlineColor, OutlineWidth))
                 {
                     pen.Alignment = PenAlignment.Inset;
                     graphics.DrawPath(pen, graphicsPath);
@@ -43,7 +88,7 @@ namespace DockPanelControler
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            DrawPath(e.Graphics);
+            Draw(e.Graphics);
         }
     }
 }
